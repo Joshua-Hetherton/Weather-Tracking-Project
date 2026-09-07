2fe4894375f2ea4050cf6cd31e28e0a45a0091e9
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-09-07 10:06:48 +0100

    Refactor: made basic response able to take in arguements (latitude and longitude)

--- weather.py
+++ weather.py
@@ -1,25 +1,27 @@
 import requests
 import pandas as pd
 
-url="https://api.open-meteo.com/v1/forecast"
-#Parameters Used in the API Request
-params = {
-    "latitude": 50.909698,
-    "longitude": -1.404351,
-    "timezone": "GMT",
-    "current": ["temperature_2m", "apparent_temperature", "precipitation", "cloud_cover", "wind_speed_10m", "wind_direction_10m", "soil_temperature_0cm"],
-    "forecast_hours":1,
-    "hourly": ["temperature_2m"]
-}
+
+def fetch_weather_data(latitude, longitude):
+    url="https://api.open-meteo.com/v1/forecast"
+    #Parameters Used in the API Request
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "timezone": "GMT",
+        "current": ["temperature_2m", "apparent_temperature", "precipitation", "cloud_cover", "wind_speed_10m", "wind_direction_10m", "soil_temperature_0cm"],
+        "forecast_hours":1,
+        "hourly": ["temperature_2m"]
+    }
 
 
-#Basic HTTP GET Request
-response=requests.get(url, params=params)
+    #Basic HTTP GET Request
+    response=requests.get(url, params=params)
 
-#200 means working, otherwise it is not working
-print(response.status_code)
+    #200 means working, otherwise it is not working
+    print(response.status_code)
 
-#Using .head() for testing with a small amount of data
-pd.DataFrame(response.json()).head()
-print(response.json())
+    #Using .head() for testing with a small amount of data
+    print(pd.DataFrame(response.json()).head())
+    print(response.json())
 
