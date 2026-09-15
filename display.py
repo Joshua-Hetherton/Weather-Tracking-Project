f700c978d7dff89bf09274bbb81044220a45f19c
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-09-15 09:01:59 +0100

    Feat:Basic Outline for display.py

---
+++ display.py
@@ -0,0 +1,15 @@
+from rich.console import Console
+from rich.table import Table
+
+def display_header():
+    pass
+
+def display_current_weather():
+    pass
+
+def display_hourly_weather():
+    pass
+
+
+console=Console()
+console.print()
