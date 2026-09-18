772efa5404feab861181d46fefb998e4bbca3d81
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-09-18 10:37:40 +0100

    Fix:Changed Font size for readability on graph

--- display.py
+++ display.py
@@ -39,34 +39,42 @@ def display_hourly_weather(hourly_weather):
 def display_graphs(hourly_weather):
     fig, ax = plt.subplots(2,2)
 
+    #Graph for temperature throughout the day
     ax[0,0].plot(hourly_weather["time"].str[11:16], hourly_weather["temperature_2m"], color="red")
+    #Title and labels for the user
     ax[0,0].set_title("Temperature (°C)")
     ax[0,0].set_xlabel("Time")
     ax[0,0].set_ylabel("Temperature (°C)")
     #Added ticks to rotate the x-axis labels for better readability
     ax[0,0].set_xticks(range(len(hourly_weather)))
-    ax[0,0].set_xticklabels(hourly_weather["time"].str[11:16], rotation=45)
+    ax[0,0].set_xticklabels(hourly_weather["time"].str[11:16], rotation=45,fontsize=8)
 
+    #Graph for cloud cover throughout the day
     ax[0,1].plot(hourly_weather["time"].str[11:16], hourly_weather["cloud_cover"], color="blue")
+    #Title and labels for the user
     ax[0,1].set_title("Cloud Cover (%)")
     ax[0,1].set_xlabel("Time")
     ax[0,1].set_ylabel("Cloud Cover (%)")
     ax[0,1].set_xticks(range(len(hourly_weather)))
-    ax[0,1].set_xticklabels(hourly_weather["time"].str[11:16], rotation=45)
+    ax[0,1].set_xticklabels(hourly_weather["time"].str[11:16], rotation=45,fontsize=8)
 
+    #Graph for wind speed throughout the day
     ax[1,0].plot(hourly_weather["time"].str[11:16], hourly_weather["wind_speed_10m"], color="green")
+    #Title and labels for the user
     ax[1,0].set_title("Wind Speed (mph)")
     ax[1,0].set_xlabel("Time")
     ax[1,0].set_ylabel("Wind Speed (mph)")
     ax[1,0].set_xticks(range(len(hourly_weather)))
-    ax[1,0].set_xticklabels(hourly_weather["time"].str[11:16], rotation=45)
+    ax[1,0].set_xticklabels(hourly_weather["time"].str[11:16], rotation=45,fontsize=8)
 
+    #Graph for precipitation throughout the day
     ax[1,1].plot(hourly_weather["time"].str[11:16], hourly_weather["precipitation"], color="purple")
+    #Title and labels for the user
     ax[1,1].set_title("Percipitation (mm)")
     ax[1,1].set_xlabel("Time")
     ax[1,1].set_ylabel("Percipitation (mm)")
     ax[1,1].set_xticks(range(len(hourly_weather)))
-    ax[1,1].set_xticklabels(hourly_weather["time"].str[11:16], rotation=45)
+    ax[1,1].set_xticklabels(hourly_weather["time"].str[11:16], rotation=45, fontsize=8)
     
 
     plt.show()
